1b9f0cf19464a0b51f9f78a48660b9964ab3d29a
qhbd <2735938147@qq.com>
2018-12-06 19:10:31 +0800

    Hello_1

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+/**
+ * HElloworlld第一次独立编写；
+ * @author Administrator
+ *未按day_01/02/03/04方式创建；
+ */
+public class Helloworld {
+	public static void main(String[] args  ){
+		System.out.println("Helloworld");
+	}
+
+}
